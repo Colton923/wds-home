1f5e4cd56579260a066a0407ce096d78f8e29cc0
Colton923 <110353948+Colton923@users.noreply.github.com>
2023-05-18 12:05:41 -0400

    .

--- src/app/api/blogCount/route.ts
+++ src/app/api/blogCount/route.ts
@@ -1,5 +1,5 @@
 import { NextResponse } from 'next/server'
-import { kv } from '@vercel/kv'
+import { createClient } from '@vercel/kv'
 import type { Blog } from 'app/blog/[id]/page'
 
 export type BlogNames = {
@@ -10,7 +10,10 @@ export type BlogNames = {
 export async function GET() {
   try {
     // this route will return how many blog posts there are, and the title of the blog post
-
+    const kv = createClient({
+      url: process.env.KV_REST_API_URL as string,
+      token: process.env.KV_REST_API_TOKEN as string,
+    })
     const blogs: Blog[] = (await kv.get('blog')) || []
     const blogNames = blogs.map((blog) => {
       return { title: blog.title, id: blog.id }
